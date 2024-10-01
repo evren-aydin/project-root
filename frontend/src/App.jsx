38eae393bb3bbbd2c8d0ae2679b5930bca01fa2f
Evren Aydın <96618847+evren-aydin@users.noreply.github.com>
2024-10-01 10:58:33 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae
+        culpa debitis sapiente numquam maxime ipsam quia itaque, vel sunt iure
+        quasi quae tenetur harum? Nesciunt officiis qui ut aperiam?
+      </p>
+    </>
+  );
+}
+
+export default App;
